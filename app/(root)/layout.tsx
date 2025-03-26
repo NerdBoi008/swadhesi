e4a68dd78f83e64e687e2b248f7353462ac4b2fc
nerdboi008 <mail.nerdboi@gmail.com>
2025-03-27 00:16:55 +0530

    Add About, Dresses, and Tops pages; integrate Notification and Credits components in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
+import Credits from "@/components/common/Creadits";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import NotifictionHeader from "@/components/common/NotifictionHeader";
 
 export default function RootLayout({
   children,
@@ -7,10 +9,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="flex gap-3 flex-col min-h-screen">
-        <Navbar />
-        {children}
-        <Footer />
-      </section>
+    <section className="flex flex-col min-h-screen">
+      
+      <NotifictionHeader variant='info'>
+        {/* <strong>Heads up!</strong> This is a demo store, you can&apos;t buy anything. */}
+      </NotifictionHeader >
+      
+      <Navbar />
+      {children}
+      <Footer />
+      <Credits/>
+    </section>
   );
 }
---
+++ app/(root)/tops/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopsPage = () => {
+  return (
+    <div>TopsPage</div>
+  )
+}
+
+export default TopsPage
